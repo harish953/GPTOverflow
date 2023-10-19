d646e258d71c91a548a5bcf2d0cc67a566494064
harish953 <patelharish953@gmail.com>
2023-10-19 16:30:29 +0530

    ignnored check ui dir

--- constants/index.ts
+++ constants/index.ts
@@ -1,48 +1,48 @@
-import { SidebarLink } from "@/types";
+import { SidebarLink } from '@/types'
 
 export const themes = [
-  { value: "light", label: "Light", icon: "/assets/icons/sun.svg" },
-  { value: "dark", label: "Dark", icon: "/assets/icons/moon.svg" },
-  { value: "system", label: "System", icon: "/assets/icons/computer.svg" },
-];
+  { value: 'light', label: 'Light', icon: '/assets/icons/sun.svg' },
+  { value: 'dark', label: 'Dark', icon: '/assets/icons/moon.svg' },
+  { value: 'system', label: 'System', icon: '/assets/icons/computer.svg' },
+]
 
 export const sidebarLinks: SidebarLink[] = [
   {
-    imgURL: "/assets/icons/home.svg",
-    route: "/",
-    label: "Home",
+    imgURL: '/assets/icons/home.svg',
+    route: '/',
+    label: 'Home',
   },
   {
-    imgURL: "/assets/icons/users.svg",
-    route: "/community",
-    label: "Community",
+    imgURL: '/assets/icons/users.svg',
+    route: '/community',
+    label: 'Community',
   },
   {
-    imgURL: "/assets/icons/star.svg",
-    route: "/collection",
-    label: "Collections",
+    imgURL: '/assets/icons/star.svg',
+    route: '/collection',
+    label: 'Collections',
   },
   {
-    imgURL: "/assets/icons/suitcase.svg",
-    route: "/jobs",
-    label: "Find Jobs",
+    imgURL: '/assets/icons/suitcase.svg',
+    route: '/jobs',
+    label: 'Find Jobs',
   },
   {
-    imgURL: "/assets/icons/tag.svg",
-    route: "/tags",
-    label: "Tags",
+    imgURL: '/assets/icons/tag.svg',
+    route: '/tags',
+    label: 'Tags',
   },
   {
-    imgURL: "/assets/icons/user.svg",
-    route: "/profile",
-    label: "Profile",
+    imgURL: '/assets/icons/user.svg',
+    route: '/profile',
+    label: 'Profile',
   },
   {
-    imgURL: "/assets/icons/question.svg",
-    route: "/ask-question",
-    label: "Ask a question",
+    imgURL: '/assets/icons/question.svg',
+    route: '/ask-question',
+    label: 'Ask a question',
   },
-];
+]
 
 export const BADGE_CRITERIA = {
   QUESTION_COUNT: {
@@ -70,4 +70,4 @@ export const BADGE_CRITERIA = {
     SILVER: 10000,
     GOLD: 100000,
   },
-};
+}
