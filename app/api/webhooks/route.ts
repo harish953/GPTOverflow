6a37cca4e756e181929a6b4bdebfc55304b5e3df
harish953 <patelharish953@gmail.com>
2023-10-23 16:40:14 +0530

    changed name of webhook secret key

--- app/api/webhooks/route.ts
+++ app/api/webhooks/route.ts
@@ -7,7 +7,7 @@ import { NextResponse } from 'next/server'
 
 export async function POST(req: Request) {
   // You can find this in the Clerk Dashboard -> Webhooks -> choose the webhook
-  const WEBHOOK_SECRET = process.env.NEXT_PUBLIC_CLERK_WEBHOOK
+  const WEBHOOK_SECRET = process.env.NEXT_CLERK_WEBHOOK_SECRET
 
   if (!WEBHOOK_SECRET) {
     throw new Error(
